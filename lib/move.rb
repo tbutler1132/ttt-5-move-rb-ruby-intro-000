f6f98296f4611842b740d9a3452a0e011dd4ba30
artificial-composition-7121 <tbutler1132@gmail.com>
2020-06-16 18:42:39 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,6 +12,6 @@ end
 
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def move(array, index, value = "X")
-  array[index] = value
+def move(board, index, value = "X")
+  board[index] = value
 end
